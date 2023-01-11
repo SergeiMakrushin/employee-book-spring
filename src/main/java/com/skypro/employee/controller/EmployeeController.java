f9850b70ab4e0f8dc0ed194b6e573456a6c74534
Сергей <mak.sergei@ngs.ru>
2023-01-11 21:26:04 +0700

    1

---
+++ src/main/java/com/skypro/employee/controller/EmployeeController.java
@@ -0,0 +1,53 @@
+package com.skypro.employee.controller;
+
+import com.skypro.employee.model.Employee;
+import com.skypro.employee.record.EmployeeRequest;
+import com.skypro.employee.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public Collection<Employee> getAllEmployees() {
+        return this.employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employee/high-salary")
+    public Collection<Employee> getHigherMediumSalary() {
+        return this.employeeService.getHigherMediumSalary();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody EmployeeRequest employeeRequest) {
+        return this.employeeService.addEmployee(employeeRequest);
+    }
+
+    @GetMapping("/employees/salary/sum")
+
+    public int getSalarySum() {
+        return this.employeeService.getSalarySum();
+    }
+
+    @GetMapping("/employees/salary/min")
+
+    public int getSalaryMin() {
+        return this.employeeService.getSalaryMin();
+    }
+
+    @GetMapping("/employees/salary/max")
+    public int getSalaryMax() {
+        return this.employeeService.getSalaryMax();
+    }
+/////////////////
+}
---
+++ src/main/java/com/skypro/employee/service/EmployeeService.java
@@ -0,0 +1,82 @@
+package com.skypro.employee.service;
+
+import com.skypro.employee.model.Employee;
+import com.skypro.employee.record.EmployeeRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class EmployeeService {
+    private final Map<Integer, Employee> employees = new HashMap<>();
+    private final Map<Integer, Employee> employees1 = new HashMap<>();
+
+    public Collection<Employee> getAllEmployees() {
+        return this.employees.values();
+    }
+
+
+    public Employee addEmployee(EmployeeRequest employeeRequest) {
+        if (employeeRequest.getFirstName() == null || employeeRequest.getLastName() == null) {
+            throw new IllegalArgumentException("Employee name should be set");
+        }
+        Employee employee = new Employee(
+                employeeRequest.getFirstName(),
+                employeeRequest.getLastName(),
+                employeeRequest.getDepartament(),
+                employeeRequest.getSalary());
+
+        this.employees.put(employee.getId(), employee);
+        return employee;
+    }
+
+    public int getSalarySum() {
+        return employees.values().stream()
+                .mapToInt(Employee::getSalary)
+                .sum();
+    }
+
+    public int getSalaryMin() {
+        Integer min = 10000000;
+        for (Employee value : employees.values()) {
+            if (value.getSalary() < min) {
+                min = value.getSalary();
+            }
+        }
+        return min;
+    }
+
+    public int getSalaryMax() {
+        Integer max = -1;
+        for (Employee value : employees.values()) {
+            if (value.getSalary() > max) {
+                max = value.getSalary();
+            }
+        }
+        return max;
+    }
+
+    public Collection<Employee> getHigherMediumSalary() {
+
+        Integer sum = 0;
+        Integer medium;
+        for (Employee value : employees.values()) {
+            sum += value.getSalary();
+        }
+        medium = sum / employees.size();
+
+        for (Map.Entry<Integer, Employee> entry : employees.entrySet()) {
+            Integer key = entry.getKey();
+            Employee value = entry.getValue();
+
+            if (value.getSalary() > medium) {
+                employees1.put(key, value);
+
+            }
+        }
+        return this.employees1.values();
+
+    }
+
+////////////////////
+}
